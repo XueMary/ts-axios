dd13d86cabece62a082084c438840c45f139f11b
薛纪洪 <632565970@qq.com>
2019-05-11 16:07:53 +0800

    error

--- src/helpers/error.ts
+++ src/helpers/error.ts
@@ -1,34 +1,34 @@
 import { AxiosRequestConfig, AxiosResponse } from '../types'
 
-// class AxiosError extends Error {
-//   config: AxiosRequestConfig
-//   code: string | null | undefined
-//   request: any
-//   response: AxiosResponse | undefined
-//   constructor(
-//     message: string,
-//     config: AxiosRequestConfig,
-//     code?: string | null,
-//     request?: any,
-//     response?: AxiosResponse
-//   ) {
-//     super(message)
+class AxiosError extends Error {
+  config: AxiosRequestConfig
+  code: string | null | undefined
+  request: any
+  response: AxiosResponse | undefined
+  constructor(
+    message: string,
+    config: AxiosRequestConfig,
+    code?: string | null,
+    request?: any,
+    response?: AxiosResponse
+  ) {
+    super(message)
 
-//     this.config = config
-//     this.code = code
-//     this.request = request
-//     this.response = response
+    this.config = config
+    this.code = code
+    this.request = request
+    this.response = response
 
-//     Object.setPrototypeOf(this, AxiosError.prototype)
-//   }
-// }
+    Object.setPrototypeOf(this, AxiosError.prototype)
+  }
+}
 
-// export function createError(
-//   message: string,
-//   config: AxiosRequestConfig,
-//   code?: string | null,
-//   request?: any,
-//   response?: AxiosResponse
-// ) {
-//   return new AxiosError(message, config, code, request, response)
-// }
+export function createError(
+  message: string,
+  config: AxiosRequestConfig,
+  code?: string | null,
+  request?: any,
+  response?: AxiosResponse
+) {
+  return new AxiosError(message, config, code, request, response)
+}
